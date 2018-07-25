08451a3b7b6a3363c27f32d55e2204a7dea43ee9
Bharat Chhabra <bharatchhabra13@gmail.com>
2018-07-25 21:12:24 +0530

    Made changes on Borrower apply for loan, Lander view, and select the loan and approved, forgot password, added 2 new packages in node nodemailer and generate-password

--- server/routes.js
+++ server/routes.js
@@ -6,6 +6,8 @@ const paths = __dirname + '/views/';
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const flash = require('connect-flash');
+var nodemailer = require('nodemailer');
+var generator = require('generate-password');
 
 const mysql = require('mysql');
 const con = mysql.createConnection({
@@ -68,11 +70,18 @@ router.get("/loan_offer", (req,res) => {
 });
 
 router.get("/retrieve_loan", (req,res) => {
-	con.query('select * from accepted', function (err, recordset) {		
-		if (err) throw err;
-		else
-			res.render('retrieve_loan', { loanList: recordset });		
-	});
+	var lender_eth_address = req.session.lender_eth_address;
+	var lender_name = req.session.lender_name;
+	if(lender_eth_address){
+		con.query('select * from accepted where lender_eth_address="'+lender_eth_address+'"', function (err, recordset) {		
+			if (err) throw err;
+			else
+				res.render('retrieve_loan', { loanList: recordset });		
+		});
+	}
+	else{
+		res.redirect('/lender_login');
+	}
 });
 
 router.get("/apply_loan", (req,res) => {
@@ -81,6 +90,21 @@ router.get("/apply_loan", (req,res) => {
 
 router.get("/lender_login", (req,res) => {
 	res.sendFile(paths + "lender_login.html");		
+});
+
+router.get("/borrowers_list", (req,res) => {
+	var lender_eth_address = req.session.lender_eth_address;
+	var lender_name = req.session.lender_name;
+	if(lender_eth_address){
+		var sql = "SELECT * FROM accepted WHERE lender_eth_address=''";
+		con.query(sql, (err, recordset) =>{
+			if(err) throw err;
+			res.render('borrowers_list', {user_name: lender_name, borrowerList: recordset});
+		});
+	}
+	else{
+		res.redirect('/lender_login');
+	}			
 });
 
 router.get("/lender_dashboard", (req,res) => {
@@ -93,6 +117,63 @@ router.get("/lender_dashboard", (req,res) => {
 	else{
 		res.redirect('/lender_login');
 	}		
+});
+
+router.get("/forgotPassword", (req,res) => {
+  res.sendFile(paths + "forgotPassword.html");
+});
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'jignasheth407@gmail.com',
+    pass: '123456'
+  }
+});
+
+router.post('/resetPassword', function(req, res, next) {
+	con.connect(function(err){
+		if(err) throw err;
+		else{
+			var email_id = req.body.lender_email;
+			var sql = "SELECT * FROM lenders WHERE username='"+email_id+"'";
+			con.query(sql, function (err2, result){
+				if(err2) throw err2;
+				else{
+					//console.log(result);
+					if(result.length > 0){
+						var n_password = generator.generate({
+							length: 7,
+							numbers: true
+						});
+						//console.log(n_password);
+						var mailOptions = {
+							from: 'jignasheth407@gmail.com',
+							to: result[0].username,
+							subject: 'Change password successful!',
+							text: 'New password is: "'+n_password+'"'
+						};
+						
+						transporter.sendMail(mailOptions, function(error, info){
+							if (error) {
+								console.log(error);
+							} else {
+								console.log('Email sent: ' + info.response);
+							}
+						});
+						var update_sql = "UPDATE lenders SET password='"+n_password+"' WHERE eth_address='"+result[0].eth_address+"'";
+						con.query(update_sql, function (err3, result3) {
+							if (err3) throw err3;
+							res.redirect('/lender_login');
+						});
+					}
+					else{
+						res.redirect('/forgotPassword');						
+					}
+				}
+			});
+		}
+	});
 });
 
 router.get("/loans_given", (req,res) => {
@@ -116,12 +197,39 @@ router.get("/story", (req,res) => {
 	var lender_eth_address = req.session.lender_eth_address;
 	var lender_name = req.session.lender_name;
 	if(lender_eth_address){
-		//res.sendFile(paths + "story.html");
-		res.render('story', { user_name: lender_name });		
+		var id = req.param('a');
+		var sql = 'select * from accepted where id="'+id+'"';
+		con.query(sql, function (err, recordset) {
+			if(recordset.length > 0){
+				if (err) 
+					console.log(err)
+				else
+					res.render('story', { user_name: lender_name, storyDetails: recordset });						
+			}
+			else{
+				res.redirect('/loans_given');							
+			}
+		});
 	}
 	else{
 		res.redirect('/lender_login');
 	}	
+});
+
+router.get("/loan_accept", (req, res) =>{
+	var lender_eth_address = req.session.lender_eth_address;
+	var lender_name = req.session.lender_name;
+	if(lender_eth_address){
+		var id = req.param('e');
+		var update_sql = "UPDATE accepted SET lender_name='"+lender_name+"', lender_eth_address='"+lender_eth_address+"' WHERE id='"+id+"'";
+		con.query(update_sql, function (err3, result3) {
+			if (err3) throw err3;
+			res.redirect('/loans_given');
+		});
+	}
+	else{
+		res.redirect('/lender_login');
+	}
 });
 
 router.get("/thanks", (req,res) => {
@@ -136,7 +244,7 @@ router.get("/logout", (req,res) => {
 router.post('/apply_loan', function(req, res, next) {
 	con.connect(function(err){
 		if(err) throw err;
-		var sql = "INSERT INTO accepted (repayment_schedule,lender_name,lender_eth_address,borrower_eth_address,amount,duration,your_story) VALUES ('yearly','Jigna','0xa609653c58c36e5fb905627fad46dd28b112504b','"+req.body.borrower_eth_address+"','"+req.body.amount+"','"+req.body.duration+"','"+req.body.your_story+"')";
+		var sql = "INSERT INTO accepted (repayment_schedule,lender_name,lender_eth_address,borrower_eth_address,amount,duration,your_story) VALUES ('yearly','','','"+req.body.borrower_eth_address+"','"+req.body.amount+"','"+req.body.duration+"','"+req.body.your_story+"')";
 		con.query(sql, function (err2, result){
 			if(err2) throw err2;
 			res.redirect('/thanks');
@@ -197,7 +305,7 @@ router.post('/signup', function(req, res, next) {
 				var sql = "INSERT INTO borrowers (eth_address,phone_number) VALUES ('"+req.body.eth_address+"','"+req.body.phone+"')";
 				con.query(sql, function(err, result){
 					if(err) throw err;
-					console.log("table created");
+					//console.log("table created");
 					res.redirect('/info');
 				});					
 			}
